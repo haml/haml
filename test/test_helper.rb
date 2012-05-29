abc3dfe166403391f981b66013291d2bf89a39fe
Norman Clarke <norman@njclarke.com>
2012-05-29 15:30:36 -0300

    Only load a Rails 3 app

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,18 +12,11 @@ require 'action_pack'
 require 'action_controller'
 require 'action_view'
 
-begin
-  require 'rails'
-  class TestApp < Rails::Application
-    config.root = ""
-  end
-  Rails.application = TestApp
-
-# For Rails 2.x
-rescue LoadError
-  require 'initializer'
-  RAILS_ROOT = ""
+require 'rails'
+class TestApp < Rails::Application
+  config.root = ""
 end
+Rails.application = TestApp
 
 ActionController::Base.logger = Logger.new(nil)
 
