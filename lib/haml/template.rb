c1ff325da832eac7c2c295cc4a5f081bb8170c04
Norman Clarke <norman@njclarke.com>
2012-05-29 16:00:08 -0300

    Always set up XSS support on Rails 3

--- lib/haml/template.rb
+++ lib/haml/template.rb
@@ -2,78 +2,31 @@ require 'haml/template/options'
 require 'haml/engine'
 require 'haml/helpers/action_view_mods'
 require 'haml/helpers/action_view_extensions'
-
-if defined?(ActionPack::VERSION::STRING) &&
-    ActionPack::VERSION::STRING == "2.3.6"
-  raise "Haml does not support Rails 2.3.6. Please upgrade to 2.3.7 or later."
-end
+require 'haml/helpers/xss_mods'
 
 module Haml
-  # The class that keeps track of the global options for Haml within Rails.
-  module Template
-    # Enables integration with the Rails 2.2.5+ XSS protection,
-    # if it's available and enabled.
-    #
-    # @return [Boolean] Whether the XSS integration was enabled.
-    def try_enabling_xss_integration
-      return false unless (ActionView::Base.respond_to?(:xss_safe?) && ActionView::Base.xss_safe?) ||
-        # We check for ActiveSupport#on_load here because if we're loading Haml that way, it means:
-        # A) we're in Rails 3 so XSS support is always on, and
-        # B) we might be in Rails 3 beta 3 where the load order is broken and xss_safe? is undefined
-        (defined?(ActiveSupport) && Haml::Util.has?(:public_method, ActiveSupport, :on_load))
-
-      Haml::Template.options[:escape_html] = true
-
-      Haml::Util.module_eval do
-        undef :rails_xss_safe? if defined? rails_xss_safe?
-        def rails_xss_safe?; true; end
-      end
-
-      require 'haml/helpers/xss_mods'
-      Haml::Helpers.send(:include, Haml::Helpers::XssMods)
-
-      Haml::Compiler.module_eval do
-        def precompiled_method_return_value_with_haml_xss
-          "::Haml::Util.html_safe(#{precompiled_method_return_value_without_haml_xss})"
-        end
-        alias_method :precompiled_method_return_value_without_haml_xss, :precompiled_method_return_value
-        alias_method :precompiled_method_return_value, :precompiled_method_return_value_with_haml_xss
-      end
-
-      true
+  module Compiler
+    def precompiled_method_return_value_with_haml_xss
+      "::Haml::Util.html_safe(#{precompiled_method_return_value_without_haml_xss})"
     end
+    alias_method :precompiled_method_return_value_without_haml_xss, :precompiled_method_return_value
+    alias_method :precompiled_method_return_value, :precompiled_method_return_value_with_haml_xss
   end
+
+  module Helpers
+    include Haml::Helpers::XssMods
+  end
+
+  module Util
+    undef :rails_xss_safe? if defined? rails_xss_safe?
+    def rails_xss_safe?; true; end
+  end
+
 end
 
-unless Haml::Util.rails_env == "development"
-  Haml::Template.options[:ugly] ||= true
-end
 
-if Haml::Util.ap_geq_3?
-  Haml::Template.options[:format] ||= :html5
-end
+Haml::Template.options[:ugly] ||= Haml::Util.rails_env == "development"
+Haml::Template.options[:escape_html] = true
+Haml::Template.options[:format] ||= :html5
 
-# Decide how we want to load Haml into Rails.
-# Patching was necessary for versions <= 2.0.1,
-# but we can make it a normal handler for higher versions.
-if defined?(ActionView::TemplateHandler) ||
-    (defined?(ActionView::Template) && defined?(ActionView::Template::Handler)) ||
-    # Rails >= 3.2.0 uses Handlers instead of Handler
-    (defined?(ActionView::Template) && defined?(ActionView::Template::Handlers))
-  require 'haml/template/plugin'
-else
-  require 'haml/template/patch'
-end
-
-# Enable XSS integration. Use Rails' after_initialize method
-# so that integration will be checked after the rails_xss plugin is loaded
-# (for Rails 2.3.* where it's not enabled by default).
-#
-# If we're running under Rails 3, though, we don't want to use after_intialize,
-# since Haml loading has already been deferred via ActiveSupport.on_load.
-if defined?(Rails.configuration.after_initialize) &&
-    !(defined?(ActiveSupport) && Haml::Util.has?(:public_method, ActiveSupport, :on_load))
-  Rails.configuration.after_initialize {Haml::Template.try_enabling_xss_integration}
-else
-  Haml::Template.try_enabling_xss_integration
-end
+require 'haml/template/plugin'
