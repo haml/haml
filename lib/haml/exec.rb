ec352d01980efa04a8bb44b0147d909949fabc40
Matt Wildig <matt@mattwildig.co.uk>
2012-11-21 00:27:54 +0000

    Delete duplicated trace message in command line

--- lib/haml/exec.rb
+++ lib/haml/exec.rb
@@ -312,7 +312,7 @@ END
           case e
           when ::Haml::SyntaxError; raise "Syntax error on line #{get_line e}: #{e.message}"
           when ::Haml::Error;       raise "Haml error on line #{get_line e}: #{e.message}"
-          else raise "Exception on line #{get_line e}: #{e.message}\n  Use --trace for backtrace."
+          else raise "Exception on line #{get_line e}: #{e.message}"
           end
         end
 
