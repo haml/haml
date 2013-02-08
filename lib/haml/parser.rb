haml/haml

commit afea81965d6cf6bb3778523fc3136c720acd6f03

String interpolations are basically faster than concatenations

lib/haml/parser.rb
@@ -186,8 +186,8 @@ module Haml
 
       def inspect
         text = "(#{type} #{value.inspect}"
-        children.each {|c| text << "\n" << c.inspect.gsub(/^/, "  ")}
-        text + ")"
+        children.each {|c| text << "\n#{c.inspect.gsub!(/^/, '  ')}"}
+        "#{text})"
       end
     end
 
@@ -571,7 +571,7 @@ module Haml
         attributes_hash, rest = balance(line, ?{, ?})
       rescue SyntaxError => e
         if line.strip[-1] == ?, && e.message == Error.message(:unbalanced_brackets)
-          line << "\n" << @next_line.text
+          line << "\n#{@next_line.text}"
           last_line += 1
           next_line
           retry
@@ -603,7 +603,7 @@ module Haml
         scanner.scan(/\s*/)
 
         if scanner.eos?
-          line << " " << @next_line.text
+          line << " #{@next_line.text}"
           last_line += 1
           next_line
           scanner.scan(/\s*/)
@@ -616,7 +616,7 @@ module Haml
         if type == :static
           static_attributes[name] = val
         else
-          dynamic_attributes << inspect_obj(name) << " => " << val << ","
+          dynamic_attributes << "#{inspect_obj(name)} => #{val},"
         end
       end
       dynamic_attributes << "}"
@@ -651,7 +651,7 @@ module Haml
 
       return name, [:static, content.first[1]] if content.size == 1
       return name, [:dynamic,
-        '"' + content.map {|(t, v)| t == :str ? inspect_obj(v)[1...-1] : "\#{#{v}}"}.join + '"']
+        %!"#{content.map {|(t, v)| t == :str ? inspect_obj(v)[1...-1] : "\#{#{v}}"}.join}"!]
     end
 
     def raw_next_line
@@ -726,7 +726,7 @@ module Haml
         break if new_line == :eod
         new_line.strip!
         next if new_line.empty?
-        text << ' ' << new_line
+        text << " #{new_line.strip}"
       end while is_ruby_multiline?(new_line)
       next_line
       text

lib/haml/compiler.rb
@@ -44,7 +44,7 @@ module Haml
     end
 
     def precompiled_with_return_value
-      precompiled + ";" + precompiled_method_return_value
+      "#{precompiled};#{precompiled_method_return_value}"
     end
 
     # Returns the precompiled string with the preamble and postamble.
@@ -66,7 +66,7 @@ ensure
 @haml_buffer = @haml_buffer.upper if @haml_buffer
 end
 END
-      preamble + locals_code(local_names) + precompiled + postamble
+      "#{preamble}#{locals_code(local_names)}#{precompiled}#{postamble}"
     end
 
     private
@@ -203,17 +203,13 @@ END
           "_hamlout.attributes(#{inspect_obj(t[:attributes])}, #{object_ref}#{attributes_hashes})")
         concat_merged_text(
           if t[:self_closing] && @options.xhtml?
-            " />" + (t[:nuke_outer_whitespace] ? "" : "\n")
+            " />#{"\n" unless t[:nuke_outer_whitespace]}"
           else
-            ">" + ((if t[:self_closing] && @options.html?
-                      t[:nuke_outer_whitespace]
-                    else
-                      !block_given? || t[:preserve_tag] || t[:nuke_inner_whitespace]
-                    end) ? "" : "\n")
+            ">#{"\n" unless (t[:self_closing] && @options.html?) ? t[:nuke_outer_whitespace] : (!block_given? || t[:preserve_tag] || t[:nuke_inner_whitespace])}"
           end)
 
         if value && !parse
-          concat_merged_text("#{value}</#{t[:name]}>#{t[:nuke_outer_whitespace] ? "" : "\n"}")
+          concat_merged_text("#{value}</#{t[:name]}>#{"\n" unless t[:nuke_outer_whitespace]}")
         elsif !t[:nuke_inner_whitespace] && !t[:self_closing]
           @to_merge << [:text, '', 1]
         end
@@ -228,7 +224,7 @@ END
         yield if block_given?
         @output_tabs -= 1 unless t[:nuke_inner_whitespace]
         rstrip_buffer! if t[:nuke_inner_whitespace]
-        push_merged_text("</#{t[:name]}>" + (t[:nuke_outer_whitespace] ? "" : "\n"),
+        push_merged_text("</#{t[:name]}>#{"\n" unless t[:nuke_outer_whitespace]}",
           t[:nuke_inner_whitespace] ? 0 : -1, !t[:nuke_inner_whitespace])
         @dont_indent_next_line = t[:nuke_outer_whitespace]
         return
@@ -236,7 +232,7 @@ END
 
       if parse
         push_script(value, t.merge(:in_tag => true))
-        concat_merged_text("</#{t[:name]}>" + (t[:nuke_outer_whitespace] ? "" : "\n"))
+        concat_merged_text("</#{t[:name]}>#{"\n" unless t[:nuke_outer_whitespace]}")
       end
     end
 

lib/haml/filters.rb
@@ -166,7 +166,7 @@ module Haml
             text = unescape_interpolation(text).gsub(/(\\+)n/) do |s|
               escapes = $1.size
               next s if escapes % 2 == 0
-              ("\\" * (escapes - 1)) + "\n"
+              "#{'\\' * (escapes - 1)}\n"
             end
             # We need to add a newline at the beginning to get the
             # filter lines to line up (since the Haml filter contains
@@ -174,7 +174,7 @@ module Haml
             # filter name). Then we need to escape the trailing
             # newline so that the whole filter block doesn't take up
             # too many.
-            text = "\n" + text.sub(/\n"\Z/, "\\n\"")
+            text = %[\n#{text.sub(/\n"\Z/, "\\n\"")}]
             push_script <<RUBY.rstrip, :escape_html => false
 find_and_preserve(#{filter.inspect}.render_with_options(#{text}, _hamlout.options))
 RUBY
@@ -213,13 +213,10 @@ RUBY
           type = " type=#{options[:attr_wrapper]}text/javascript#{options[:attr_wrapper]}"
         end
 
-        str = "<script#{type}>\n"
-        str << "  //<![CDATA[\n" if options[:cdata]
-        str << "#{indent}#{text.rstrip.gsub("\n", "\n#{indent}")}\n"
-        str << "  //]]>\n" if options[:cdata]
-        str << "</script>"
+        text = text.rstrip
+        text.gsub!("\n", "\n#{indent}")
 
-        str
+        %!<script#{type}>\n#{"  //<![CDATA[\n" if options[:cdata]}#{indent}#{text}\n#{"  //]]>\n" if options[:cdata]}</script>!
       end
     end
 
@@ -237,13 +234,10 @@ RUBY
           type = " type=#{options[:attr_wrapper]}text/css#{options[:attr_wrapper]}"
         end
 
-        str = "<style#{type}>\n"
-        str << "  /*<![CDATA[*/\n" if options[:cdata]
-        str << "#{indent}#{text.rstrip.gsub("\n", "\n#{indent}")}\n"
-        str << "  /*]]>*/\n" if options[:cdata]
-        str << "</style>"
+        text = text.rstrip
+        text.gsub!("\n", "\n#{indent}")
 
-        str
+        %(<style#{type}>\n#{"  /*<![CDATA[*/\n" if options[:cdata]}#{indent}#{text}\n#{"  /*]]>*/\n" if options[:cdata]}</style>)
       end
     end
 
@@ -253,7 +247,10 @@ RUBY
 
       # @see Base#render
       def render(text)
-        "<![CDATA[#{("\n" + text).rstrip.gsub("\n", "\n    ")}\n]]>"
+        text = "\n#{text}"
+        text.rstrip!
+        text.gsub!("\n", "\n    ")
+        "<![CDATA[#{text}\n]]>"
       end
     end
 

lib/haml/helpers.rb
@@ -397,10 +397,9 @@ MESSAGE
     # @param text [#to_s] The text to output
     def haml_concat(text = "")
       unless haml_buffer.options[:ugly] || haml_indent == 0
-        haml_buffer.buffer << haml_indent <<
-          text.to_s.gsub("\n", "\n" + haml_indent) << "\n"
+        haml_buffer.buffer << %[#{haml_indent}#{text.to_s.gsub("\n", "\n#{haml_indent}")}\n]
       else
-        haml_buffer.buffer << text.to_s << "\n"
+        haml_buffer.buffer << "#{text}\n"
       end
       ErrorReturn.new("haml_concat")
     end
@@ -503,7 +502,7 @@ MESSAGE
           tab_down
           haml_concat "</#{name}>"
         else
-          tag << text << "</#{name}>"
+          tag << "#{text}</#{name}>"
           haml_concat tag
         end
         return ret
@@ -514,7 +513,7 @@ MESSAGE
       end
 
       if flags.include?(:<)
-        tag << capture_haml(&block).strip << "</#{name}>"
+        tag << "#{capture_haml(&block).strip}</#{name}>"
         haml_concat tag
         return ret
       end

lib/haml/helpers/action_view_extensions.rb
@@ -32,7 +32,7 @@ module Haml
       #
       # @return [String] The class name for the current page
       def page_class
-        controller.controller_name + " " + controller.action_name
+        "#{controller.controller_name} #{controller.action_name}"
       end
       alias_method :generate_content_class_names, :page_class
 

commit 190532b40a5eced55de9a0aae04ec5841afa88af

Broken in afea819 :bomb:

lib/haml/parser.rb
@@ -185,9 +185,7 @@ module Haml
       end
 
       def inspect
-        text = "(#{type} #{value.inspect}"
-        children.each {|c| text << "\n#{c.inspect.gsub!(/^/, '  ')}"}
-        "#{text})"
+        %Q[(#{type} #{value.inspect}\n#{children.map {|c| c.inspect.gsub!(/^/, '  ')}.join("\n")} #{text})]
       end
     end
 