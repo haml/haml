d8a260b09b44aaa22703af686d4b17e392f0b308
Norman Clarke <norman@njclarke.com>
2013-02-12 23:16:55 -0300

    Remove unneeded require, brought doc up to date

--- lib/haml/helpers/rails_323_textarea_fix.rb
+++ lib/haml/helpers/rails_323_textarea_fix.rb
@@ -1,26 +1,9 @@
-require "abstract_controller/rendering"
-
-# Rails 3.2.3's form helpers add a newline after opening textareas, which can
-# cause problems with newlines being considered content rather than markup.
-# These changes fix the issue by making the helpers emit "<haml:newline/>"
-# rather than the leading newline. The tag is then replaced by a newline after
-# rendering.
-#
-# This should be considered nothing more than an emergency hotfix to ensure
-# compatibility with the latest version of Rails, made at a moment when the Haml
-# project is transitioning to a new maintainer.
-
-# module AbstractController
-#   module Rendering
-#     def render_to_body_with_haml(options = {})
-#       if rendered = render_to_body_without_haml(options)
-#         rendered.gsub('<haml:newline/>', "\n").html_safe
-#       end
-#     end
-#     alias_method_chain :render_to_body, :haml
-#   end
-# end
-
+# As of 3.2.3, Rails's form helpers add a newline after opening textareas,
+# which can cause problems with newlines being considered content rather than
+# markup. Here we decode the first newline back into a real newline to make the
+# textarea work as expected. Note that in order for this to work, the compiler
+# code was also change to avoid indenting code with textareas, even when Haml
+# is running in indented mode.
 module ActionView
   module Helpers
 
