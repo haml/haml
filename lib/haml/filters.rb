a45154af0bf869e671c4ab733153a046be91f1b0
Matt Wildig <matt@mattwildig.co.uk>
2013-03-21 22:32:06 +0000

    Require 'erubis' if needed in erb filter
    Requiring erubis/tiny can cause the erb fiter to fail if erubis hasn't
    been explicitly required elsewhere.
    
    See https://github.com/rtomayko/tilt/pull/183
    
    As a workaround require 'erubis' if needed when compiling the erb
    filter.

--- lib/haml/filters.rb
+++ lib/haml/filters.rb
@@ -385,6 +385,8 @@ RUBY
     module Erb
       class << self
         def precompiled(text)
+          #workaround for https://github.com/rtomayko/tilt/pull/183
+          require 'erubis' if (defined?(::Erubis) && !defined?(::Erubis::Eruby))
           super.sub(/^#coding:.*?\n/, '')
         end
       end
