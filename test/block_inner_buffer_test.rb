87cca9111cb6ffe6df67969708dd3907e1680462
Iazel <pelligra.s@gmail.com>
2014-09-15 02:01:11 +0200

    Blocks become more useful

---
+++ test/block_inner_buffer_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class BlockInnerBufferTest < Haml::TestCase
+  def test_block_inner_buffer
+    haml = clean_space <<-HAML
+    Hello
+    = simple_tag('em') do
+      = 'World'.upcase
+    !!
+    HAML
+
+    assert_equal "Hello\n<em>WORLD</em>\n!!\n", render(haml, {}, TestContext.new)
+  end
+
+  def clean_space(str)
+    str.gsub /^#{str.scan(/^\s+/).min}/, ''
+  end
+
+  class TestContext
+    def simple_tag(tag)
+      "<#{tag}>#{yield}</#{tag}>"
+    end
+  end
+end
