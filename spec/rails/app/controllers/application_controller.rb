fa45c34bc64d0311046c9b35d2440b3a38898707
Takashi Kokubun <takashikkbn@gmail.com>
2015-03-12 23:15:13 +0900

    Use local hamlit in dummy app

--- spec/rails/app/controllers/application_controller.rb
+++ spec/rails/app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def index
+  end
 end
