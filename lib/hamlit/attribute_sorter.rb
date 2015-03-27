cc6295d400d03f0cb3847b8727bc1317c3e849d1
Takashi Kokubun <takashikkbn@gmail.com>
2015-03-27 22:38:31 +0900

    Fix a comment

--- lib/hamlit/attribute_sorter.rb
+++ lib/hamlit/attribute_sorter.rb
@@ -2,7 +2,7 @@ require 'hamlit/filter'
 
 # Haml-spec specifies attribute order inspite of an original order.
 # Because temple's parser should return a result as original as possible,
-# the ordering delegated to this filter.
+# ordering of it is delegated to this filter.
 module Hamlit
   class AttributeSorter < Hamlit::Filter
     def on_haml_attrs(*attrs)
