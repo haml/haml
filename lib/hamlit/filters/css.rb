e238352fbccd04be3d91d3ef2ac77139cc527f6c
Takashi Kokubun <takashikkbn@gmail.com>
2015-03-29 03:59:46 +0900

    Compile css filter for xhtml

--- lib/hamlit/filters/css.rb
+++ lib/hamlit/filters/css.rb
@@ -4,10 +4,30 @@ module Hamlit
   module Filters
     class Css < Base
       def compile(lines)
+        return compile_xhtml(lines) if options[:format] == :xhtml
+
+        compile_html(lines)
+      end
+
+      private
+
+      def compile_html(lines)
         ast = [:haml, :text, compile_lines(lines, indent_width: 2)]
         ast = [:multi, [:static, "\n"], ast]
         ast = [:html, :tag, 'style', [:html, :attrs], ast]
         ast
+      end
+
+      def compile_xhtml(lines)
+        attr  = [:html, :attr, 'type', [:static, 'text/css']]
+        attrs = [:html, :attrs, attr]
+
+        multi = [:multi, [:static, "\n"]]
+        multi << [:static, "  /*<![CDATA[*/\n"]
+        multi << [:haml, :text, compile_lines(lines, indent_width: 4)]
+        multi << [:static, "  /*]]>*/\n"]
+
+        [:html, :tag, 'style', attrs, multi]
       end
     end
   end
