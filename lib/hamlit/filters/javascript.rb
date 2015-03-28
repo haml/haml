da2281473fa95c98f537e4453c70dd0d8e2a018e
Takashi Kokubun <takashikkbn@gmail.com>
2015-03-29 03:55:49 +0900

    Compile javascript filter for xhtml

--- lib/hamlit/filters/javascript.rb
+++ lib/hamlit/filters/javascript.rb
@@ -4,10 +4,30 @@ module Hamlit
   module Filters
     class Javascript < Base
       def compile(lines)
+        return compile_xhtml(lines) if options[:format] == :xhtml
+
+        compile_html(lines)
+      end
+
+      private
+
+      def compile_html(lines)
         ast = [:haml, :text, compile_lines(lines, indent_width: 2)]
         ast = [:multi, [:static, "\n"], ast]
         ast = [:html, :tag, 'script', [:html, :attrs], ast]
         ast
+      end
+
+      def compile_xhtml(lines)
+        attr  = [:html, :attr, 'type', [:static, 'text/javascript']]
+        attrs = [:html, :attrs, attr]
+
+        multi = [:multi, [:static, "\n"]]
+        multi << [:static, "  //<![CDATA[\n"]
+        multi << [:haml, :text, compile_lines(lines, indent_width: 4)]
+        multi << [:static, "  //]]>\n"]
+
+        [:html, :tag, 'script', attrs, multi]
       end
     end
   end
