2cd0407d1c546afdbe85d8594c87b6da2555dfda
Takashi Kokubun <takashikkbn@gmail.com>
2015-05-31 21:01:51 +0900

    Escape interpolated string

--- lib/hamlit/compilers/text.rb
+++ lib/hamlit/compilers/text.rb
@@ -1,14 +1,91 @@
+require 'hamlit/concerns/escapable'
+require 'hamlit/concerns/included'
 require 'hamlit/concerns/string_interpolation'
 
 module Hamlit
   module Compilers
     module Text
+      extend Concerns::Included
       include Concerns::StringInterpolation
 
-      def on_haml_text(exp)
-        return [:static, exp] unless contains_interpolation?(exp)
+      included do
+        include Concerns::Escapable
+      end
 
-        [:dynamic, string_literal(exp)]
+      # To ask ripper to consider a given text as string literal,
+      # I change "foo" to "%!foo!".
+      # This constant is the candidates for the literal surrounder.
+      STRING_MARKERS = %w[' " ! @ $ % ^ & * | =].freeze
+
+      # Return static and dynamic temple ast.
+      # It splits expression to optimize because string interpolation is slow.
+      def on_haml_text(exp)
+        return static_text(exp) unless contains_interpolation?(exp)
+
+        marker = find_string_marker(exp)
+        return [:dynamic, string_literal(exp)] unless marker
+
+        open_pos, close_pos = find_interpolation(exp, marker)
+        return static_text(exp) unless open_pos && close_pos
+
+        pre  = exp.byteslice(0...open_pos)
+        body = exp.byteslice((open_pos + 2)...close_pos)
+        post = exp.byteslice((close_pos + 1)...exp.bytesize)
+        [:multi, [:static, pre], escape_html([:dynamic, body]) on_haml_text(post)]
+      end
+
+      def find_interpolation(exp, marker)
+        return unless contains_interpolation?(exp)
+
+        offset     = 2 # 2 is the length of '%' and marker
+        open_pos   = nil
+        close_pos  = nil
+        open_count = 0
+        literal    = literalify_string(exp, marker)
+
+        Ripper.lex(literal).each do |(row, col), type, str|
+          case type
+          when :on_embexpr_beg
+            open_pos = calc_position(exp, row, col, offset) if open_count == 0
+            open_count += 1
+          when :on_embexpr_end
+            open_count -= 1
+            return [open_pos, calc_position(exp, row, col, offset)] if open_count == 0
+          end
+          open_count
+        end
+
+        nil
+      end
+
+      private
+
+      # :static can't treat backslash properly
+      def static_text(exp)
+        return [:dynamic, string_literal(exp)] if exp.include?('\\')
+        [:static, exp]
+      end
+
+      def find_string_marker(text)
+        STRING_MARKERS.each do |marker|
+          return marker unless text.include?(marker)
+        end
+        nil
+      end
+
+      def literalify_string(exp, marker)
+        "%#{marker}#{exp}#{marker}"
+      end
+
+      def calc_position(exp, row, col, offset)
+        return col - offset if row <= 1
+
+        pos   = col
+        lines = exp.split("\n")
+        (0..(row - 2)).each do |row_index|
+          pos += lines[row_index].bytesize + 1
+        end
+        pos
       end
     end
   end
--- spec/hamlit/engine/text_spec.rb
+++ spec/hamlit/engine/text_spec.rb
@@ -6,8 +6,8 @@ describe Hamlit::Engine do
         a#{{ a: 3 }}
         <ht#{2}ml>
       HAML
-        a3aa" ["1", 2] b " !
-        a{:a=>3}
+        a3aa" [&quot;1&quot;, 2] b " !
+        a{:a=&gt;3}
         <ht2ml>
       HTML
     end
@@ -41,6 +41,22 @@ describe Hamlit::Engine do
 
     it 'leaves empty spaces after backslash' do
       expect(render_string('\       a')).to eq("       a\n")
+    end
+
+    describe 'string interpolation' do
+      specify { assert_render('#{}', "\n") }
+      specify { assert_render('1#{}', "1\n") }
+      specify { assert_render('1#{2}', "12\n") }
+      specify { assert_render('1#{2', "1\#{2\n") }
+      specify { assert_render('}#{1}', "}1\n") }
+      specify { assert_render('#{1}2', "12\n") }
+      specify { assert_render('1#{ "2#{3}4" }5', "12345\n") }
+      specify { assert_render('#{1}2#{3}4#{5}6#{7}8#{9}', "123456789\n") }
+      specify { assert_render(%q{'"!@$%^&*|=#{1}1#{1}2}, %Q{'"!@$%^&*|=1112\n}) }
+      specify { assert_render('あ#{1}', "あ1\n") }
+      specify { assert_render('あ#{"い"}う', "あいう\n") }
+      specify { assert_render('a#{"<b>"}c', "a&lt;b&gt;c\n") }
+      specify { assert_render(":plain\n  あ\n  \#{'い'}", "あ\nい\n\n") }
     end
   end
 end
