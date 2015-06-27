7a52e9624dac2530b357619c5bcfe0b6f27a674f
Takashi Kokubun <takashikkbn@gmail.com>
2015-06-27 14:38:49 +0900

    Refactor benchmark.rb

--- benchmarks/benchmark.rb
+++ benchmarks/benchmark.rb
@@ -12,22 +12,15 @@ require 'slim'
 require 'hamlit'
 
 class Benchmarks
-  def initialize(time)
+  def initialize(time, escape_enabled)
     @time     = time
     @benches  = []
     @versions = {}
-  end
 
-  def init_compiled_benches(escape_enabled)
+    init_compiled_benches(escape_enabled)
     if escape_enabled
-      @erb_code  = File.read(File.dirname(__FILE__) + '/view.erb')
-      @haml_code = File.read(File.dirname(__FILE__) + '/view.haml')
-      @slim_code = File.read(File.dirname(__FILE__) + '/view.escaped.slim')
       init_escaped_benches
     else
-      @erb_code  = File.read(File.dirname(__FILE__) + '/view.erb')
-      @haml_code = File.read(File.dirname(__FILE__) + '/view.haml')
-      @slim_code = File.read(File.dirname(__FILE__) + '/view.slim')
       init_plain_benches
     end
   end
@@ -45,18 +38,18 @@ class Benchmarks
 
   private
 
+  def init_compiled_benches(escape_enabled)
+    slim_path  = "/view#{'.escaped' if escape_enabled}.slim"
+    @erb_code  = File.read(File.dirname(__FILE__) + '/view.erb')
+    @haml_code = File.read(File.dirname(__FILE__) + '/view.haml')
+    @slim_code = File.read(File.dirname(__FILE__) + slim_path)
+  end
+
   # Totally the same as slim-template/slim's compiled bench.
   def init_plain_benches
     puts 'Compiled rendering benchmarks without HTML escape'
     context = Context.new
-
-    haml_ugly = Haml::Engine.new(@haml_code, format: :html5, ugly: true)
-    haml_ugly.def_method(context, :run_haml_ugly)
-    context.instance_eval %{
-      def run_erubis; #{Erubis::Eruby.new(@erb_code).src}; end
-      def run_slim_ugly; #{Slim::Engine.new.call @slim_code}; end
-      def run_hamlit; #{Hamlit::Engine.new(escape_html: false).call @haml_code}; end
-    }
+    define_plain_methods!(context)
 
     bench('hamlit', Hamlit::VERSION) { context.run_hamlit }
     bench('erubis', Erubis::VERSION) { context.run_erubis }
@@ -64,12 +57,31 @@ class Benchmarks
     bench('haml', Haml::VERSION)     { context.run_haml_ugly }
   end
 
+  def define_plain_methods!(context)
+    haml_ugly = Haml::Engine.new(@haml_code, format: :html5, ugly: true)
+    haml_ugly.def_method(context, :run_haml_ugly)
+    context.instance_eval %{
+      def run_erubis; #{Erubis::Eruby.new(@erb_code).src}; end
+      def run_slim_ugly; #{Slim::Engine.new.call @slim_code}; end
+      def run_hamlit; #{Hamlit::Engine.new(escape_html: false).call @haml_code}; end
+    }
+  end
+
   # slim-template/slim's compiled bench with HTML escaping.
   def init_escaped_benches
     puts 'Compiled rendering benchmarks with HTML escape'
     context = Context.new
-    context.instance_eval("def header; '<script>'; end")
+    define_escaped_methods!(context)
 
+    bench('hamlit', Hamlit::VERSION) { context.run_hamlit }
+    bench('slim', Slim::VERSION)     { context.run_slim_ugly }
+    bench('faml', Faml::VERSION)     { context.run_faml }
+    bench('erubis', Erubis::VERSION) { context.run_erubis }
+    bench('haml', Haml::VERSION)     { context.run_haml_ugly }
+  end
+
+  def define_escaped_methods!(context)
+    context.instance_eval("def header; '<script>'; end")
     haml_ugly = Haml::Engine.new(@haml_code, format: :html5, ugly: true, escape_html: true)
     haml_ugly.def_method(context, :run_haml_ugly)
     context.instance_eval %{
@@ -78,12 +90,6 @@ class Benchmarks
       def run_faml; #{Faml::Engine.new.call @haml_code}; end
       def run_hamlit; #{Hamlit::Engine.new.call @haml_code}; end
     }
-
-    bench('hamlit', Hamlit::VERSION) { context.run_hamlit }
-    bench('slim', Slim::VERSION)     { context.run_slim_ugly }
-    bench('faml', Faml::VERSION)     { context.run_faml }
-    bench('erubis', Erubis::VERSION) { context.run_erubis }
-    bench('haml', Haml::VERSION)     { context.run_haml_ugly }
   end
 
   def bench(name, version, &block)
@@ -100,6 +106,5 @@ class Benchmarks
 end
 
 time = (ENV['TIME'] || 5).to_i
-bench = Benchmarks.new(time)
-bench.init_compiled_benches(ENV['HTML_ESCAPE'])
+bench = Benchmarks.new(time, ENV['HTML_ESCAPE'] == '1')
 bench.run
