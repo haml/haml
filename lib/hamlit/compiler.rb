8f6436ca9f70a1b9fc607a77b297f66262ec4b68
Takashi Kokubun <takashikkbn@gmail.com>
2015-10-07 00:33:20 +0900

    Compile basic tag

--- lib/hamlit/compiler.rb
+++ lib/hamlit/compiler.rb
@@ -4,8 +4,22 @@ module Hamlit
       @options = options
     end
 
-    def call(template)
-      [:multi, [:static, 'Hamlit']]
+    def call(ast)
+      temple = ast.children.map do |node|
+        compile_node(node)
+      end
+      [:multi, *temple]
+    end
+
+    private
+
+    def compile_node(node)
+      case node.type
+      when :tag
+        [:html, :tag, node.value[:name], [:html, :attrs], [:multi]]
+      else
+        [:static, "\n"]
+      end
     end
   end
 end
