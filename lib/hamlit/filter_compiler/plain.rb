07492e715260a439e65e855fe4eb247dc72d6a84
Takashi Kokubun <takashikkbn@gmail.com>
2015-10-12 08:38:15 +0900

    Allow interpolation in plain

--- lib/hamlit/filter_compiler/plain.rb
+++ lib/hamlit/filter_compiler/plain.rb
@@ -1,8 +1,15 @@
+require 'haml/util'
+
 module Hamlit
   class FilterCompiler
     class Plain
       def compile(node)
-        [:static, node.value[:text].rstrip + "\n"]
+        text = node.value[:text].rstrip + "\n"
+        if Haml::Util.contains_interpolation?(text)
+          [:dynamic, Haml::Util.unescape_interpolation(text + "\n")]
+        else
+          [:static, text]
+        end
       end
     end
   end
