4967bfae99004569bc3446e1d819188782f53424
Takashi Kokubun <takashikkbn@gmail.com>
2015-10-28 22:12:12 +0900

    Refactor less and coffee

--- lib/hamlit/filters/coffee.rb
+++ lib/hamlit/filters/coffee.rb
@@ -1,29 +1,10 @@
-require 'tilt'
-
 module Hamlit
   class Filters
     class Coffee < TiltBase
       def compile(node)
-        if Haml::Util.contains_interpolation?(node.value[:text])
-          return runtime_compile(node)
-        end
-
         temple = [:multi]
         temple << [:static, "<script>\n".freeze]
-        temple << [:static, TiltBase.render('coffee', node.value[:text])]
-        temple << [:static, "</script>".freeze]
-        temple
-      end
-
-      private
-
-      def runtime_compile(node)
-        source = Haml::Util.unescape_interpolation(node.value[:text])
-        code = "::Hamlit::Filters::TiltBase.render('coffee', #{source})"
-
-        temple = [:multi]
-        temple << [:static, "<script>\n".freeze]
-        temple << [:dynamic, code]
+        temple << compile_with_tilt(node, 'coffee')
         temple << [:static, "</script>".freeze]
         temple
       end
--- lib/hamlit/filters/tilt_base.rb
+++ lib/hamlit/filters/tilt_base.rb
@@ -1,9 +1,30 @@
+require 'tilt'
+
 module Hamlit
   class Filters
     class TiltBase < Base
       def self.render(name, source)
         text = ::Tilt["t.#{name}"].new { source }.render
         text.gsub!(/^/, '  '.freeze)
+      end
+
+      private
+
+      def compile_with_tilt(node, name)
+        if Haml::Util.contains_interpolation?(node.value[:text])
+          dynamic_compile(node, name)
+        else
+          static_compile(node, name)
+        end
+      end
+
+      def static_compile(node, name)
+        [:static, TiltBase.render(name, node.value[:text])]
+      end
+
+      def dynamic_compile(node, name)
+        source = Haml::Util.unescape_interpolation(node.value[:text])
+        [:dynamic, "::Hamlit::Filters::TiltBase.render('#{name}', #{source})"]
       end
     end
   end
