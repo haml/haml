c7f4919926b01f47a480ed1f681d00e21ba0fb1b
Takashi Kokubun <takashikkbn@gmail.com>
2015-11-03 10:30:44 +0900

    Block compilation differs

--- benchmark/real_sample.rb
+++ benchmark/real_sample.rb
@@ -2,13 +2,8 @@ def render(*)
   '<div class="render"></div>'
 end
 
-def link_to(*args, &block)
-  a, b, *c = args
-  if block_given?
-    "<a href='" << a << ">".freeze << yield.to_s << '</div>'.freeze
-  else
-    "<a href='" << a << ">".freeze << b.to_s << '</div>'.freeze
-  end
+def link_to(a, b, *c)
+  "<a href='" << b << ">".freeze << a << '</div>'.freeze
 end
 
 def image_tag(*)
