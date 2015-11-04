df14cc4fef42fe842ffee63e1efaaa2b2866e3bb
Takashi Kokubun <takashikkbn@gmail.com>
2015-11-04 21:41:35 +0900

    Rollback optimized code

--- lib/hamlit/compiler/attribute_compiler.rb
+++ lib/hamlit/compiler/attribute_compiler.rb
@@ -14,7 +14,6 @@ module Hamlit
         [:html, :attrs].tap do |temple|
           case
           when !node.value[:attributes_hashes].empty?
-            # compile_dynamic_attributes!(temple, node)
             compile_on_runtime!(temple, node)
           when !node.value[:attributes].empty?
             compile_static_attributes!(temple, node)
@@ -24,29 +23,10 @@ module Hamlit
 
       private
 
-      def compile_dynamic_attributes!(temple, node)
-        hashes = []
-        node.value[:attributes_hashes].each do |hash_str|
-          hash = HashParser.parse(hash_str)
-          return compile_on_runtime!(temple, node) unless hash
-          hashes << hash
-        end
-        hashes.unshift(node.value[:attributes]) if node.value[:attributes] != {}
-        compile_statically!(temple, hashes)
-      end
-
       def compile_on_runtime!(temple, node)
         attrs = node.value[:attributes_hashes]
         attrs.unshift(node.value[:attributes].inspect) if node.value[:attributes] != {}
         temple << [:dynamic, "::Hamlit::AttributeBuilder.build(#{@quote}, #{@format.inspect}, #{attrs.join(', ')})"]
-      end
-
-      def compile_statically!(temple, hashes)
-        hashes.each do |hash|
-          hash.each do |key, value|
-            temple << [:html, :attr, key, [:escape, @escape_attrs, [:dynamic, value]]]
-          end
-        end
       end
 
       def compile_static_attributes!(temple, node)
