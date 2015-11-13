8075ebf6be8e507cefd2698b8d047a9a3108df98
Takashi Kokubun <takashikkbn@gmail.com>
2015-11-13 23:27:20 +0900

    Sort attributes test

--- test/hamlit/engine/attributes_test.rb
+++ test/hamlit/engine/attributes_test.rb
@@ -1,15 +1,6 @@
 describe Hamlit::Engine do
   include RenderAssertion
 
-  it { assert_haml(%q|%div|) }
-  it { assert_inline(%Q|.bar.foo|) }
-  it { assert_inline(%Q|.foo.bar|) }
-  it { assert_inline(%Q|%div(class='bar foo')|) }
-  it { assert_inline(%Q|%div(class='foo bar')|) }
-  it { assert_inline(%Q|%div{ class: 'bar foo' }|) }
-  it { assert_render(%q|%a{ href: "'\"" }|, %Q|<a href='&#39;&quot;'></a>\n|) }
-  it { assert_inline(%Q|%a{ href: '/search?foo=bar&hoge=<fuga>' }|) }
-
   describe 'id attributes' do
     specify 'compatilibity' do
       assert_haml(<<-HAML)
@@ -47,39 +38,56 @@ describe Hamlit::Engine do
     end
   end
 
-  specify 'class attributes' do
-    assert_haml(<<-HAML)
-      - klass = 'b a'
-      .b.a
-      .b{ class: 'a' }
-      .a{ class: 'b a' }
-      .b.a{ class: 'b a' }
-      .b{ class: 'b a' }
-      .a{ class: klass }
-      .b{ class: klass }
-      .b.a{ class: klass }
-      .b{ class: 'c a' }
-      .b{ class: 'a c' }
-      .a{ class: [] }
-      .a{ class: %w[c b] }
-      %div{ class: 'b a' }(class=klass)
-      %div(class=klass){ class: 'b a' }
-      .a.d(class=klass){ class: 'c d' }
-      .a.d(class=klass)
-      .a.c(class='b')
-      - klass = nil
-      .a{:class => nil}
-      .a{:class => false}
-      .a{:class => klass}
-      .a{:class => nil}(class=klass)
-      - hash = { class: nil }
-      .b{ hash, class: 'a' }
-      .b{ hash, 'class' => 'a' }
-      - hash = { class: 'd' }
-      .a{ hash }
-      .b{ hash, class: 'a' }(class='c')
-      .b{ hash, class: 'a' }(class=klass)
-    HAML
+  describe 'class attributes' do
+    specify 'compatibility' do
+      assert_inline(%q|.bar.foo|)
+      assert_inline(%q|.foo.bar|)
+      assert_inline(%Q|%div(class='bar foo')|)
+      assert_inline(%Q|%div(class='foo bar')|)
+      assert_inline(%Q|%div{ class: 'bar foo' }|)
+      assert_haml(<<-HAML)
+        - klass = 'b a'
+        .b.a
+        .b{ class: 'a' }
+        .a{ class: 'b a' }
+        .b.a{ class: 'b a' }
+        .b{ class: 'b a' }
+        .a{ class: klass }
+        .b{ class: klass }
+        .b.a{ class: klass }
+        .b{ class: 'c a' }
+        .b{ class: 'a c' }
+        .a{ class: [] }
+        .a{ class: %w[c b] }
+        %div{ class: 'b a' }(class=klass)
+        %div(class=klass){ class: 'b a' }
+        .a.d(class=klass){ class: 'c d' }
+        .a.d(class=klass)
+        .a.c(class='b')
+        - klass = nil
+        .a{:class => nil}
+        .a{:class => false}
+        .a{:class => klass}
+        .a{:class => nil}(class=klass)
+        - hash = { class: nil }
+        .b{ hash, class: 'a' }
+        .b{ hash, 'class' => 'a' }
+        - hash = { class: 'd' }
+        .a{ hash }
+        .b{ hash, class: 'a' }(class='c')
+        .b{ hash, class: 'a' }(class=klass)
+      HAML
+    end
+
+    specify 'incompatibility' do
+      assert_render(%q|%div{ class: 'b a' }|, %Q|<div class='a b'></div>\n|)
+      assert_render(<<-HAML, <<-HTML)
+        - klass = 'b a'
+        %div{ class: klass }
+      HAML
+        <div class='a b'></div>
+      HTML
+    end
   end
 
   specify 'data attributes' do
@@ -141,27 +149,23 @@ describe Hamlit::Engine do
     assert_inline(%q|%input(checked=true)|, format: :xhtml)
   end
 
-  specify 'common attributes' do
-    assert_haml(<<-HAML)
-      - new = 'new'
-      - old = 'old'
-      %span(foo='new'){ foo: 'old' }
-      %span{ foo: 'old' }(foo='new')
-      %span(foo=new){ foo: 'old' }
-      %span{ foo: 'old' }(foo=new)
-      %span(foo=new){ foo: old }
-      %span{ foo: old }(foo=new)
-    HAML
-  end
+  describe 'common attributes' do
+    specify 'compatibility' do
+      assert_inline(%Q|%a{ href: '/search?foo=bar&hoge=<fuga>' }|)
+      assert_haml(<<-HAML)
+        - new = 'new'
+        - old = 'old'
+        %span(foo='new'){ foo: 'old' }
+        %span{ foo: 'old' }(foo='new')
+        %span(foo=new){ foo: 'old' }
+        %span{ foo: 'old' }(foo=new)
+        %span(foo=new){ foo: old }
+        %span{ foo: old }(foo=new)
+      HAML
+    end
 
-  specify 'incompatibility' do
-    assert_render(<<-HAML, <<-HTML)
-      - klass = 'b a'
-      %div{ class: 'b a' }
-      %div{ class: klass }
-    HAML
-      <div class='a b'></div>
-      <div class='a b'></div>
-    HTML
+    specify 'incompatibility' do
+      assert_render(%q|%a{ href: "'\"" }|, %Q|<a href='&#39;&quot;'></a>\n|)
+    end
   end
 end
