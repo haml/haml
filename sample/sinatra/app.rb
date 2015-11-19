0aa5dbda0083e21a6a02e4c3ea2dca762322ecc1
Takashi Kokubun <takashikkbn@gmail.com>
2015-11-19 14:55:55 +0900

    Add sinatra sample

---
+++ sample/sinatra/app.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+
+User = Struct.new(:id, :name)
+
+set :haml, format: :html5
+
+get '/' do
+  @notice = 'hello'
+  @users = [
+    User.new(1, 'k0kubun'),
+    User.new(2, 'hello'),
+  ]
+  haml :index
+end
+
+get '/center' do
+  @notice = 'centering'
+  @users = [User.new(3, 'neko')]
+  haml :index, layout: :center
+end
