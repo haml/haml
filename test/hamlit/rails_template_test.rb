bb40ac160488bba9930da44f00bded69650dafe0
Takashi Kokubun <takashikkbn@gmail.com>
2015-11-19 13:30:51 +0900

    Test helpers on ActionView

--- test/hamlit/rails_template_test.rb
+++ test/hamlit/rails_template_test.rb
@@ -3,25 +3,36 @@ require 'haml/template/plugin'
 require 'hamlit/rails_template'
 
 describe Hamlit::RailsTemplate do
-  def render(haml, with: :hamlit)
-    case with
-    when :hamlit
-      ActionView::Template.register_template_handler :haml, Hamlit::RailsTemplate.new
-    else
-      Haml::Template.options[:ugly] = true
-      ActionView::Template.register_template_handler :haml, Haml::Plugin
-    end
-
+  def render(haml)
+    ActionView::Template.register_template_handler(:haml, Hamlit::RailsTemplate.new)
     base = ActionView::Base.new(__dir__, {})
     base.render(inline: haml, type: :haml)
   end
 
-  specify 'rails rendering' do
+  specify 'html escape' do
+    assert_equal %Q|&lt;script&gt;alert(&quot;a&quot;);&lt;/script&gt;\n|, render(<<-HAML.unindent)
+      = '<script>alert("a");</script>'
+    HAML
+  end
+
+  specify 'attribute escape' do
+    assert_equal %Q|<a href='&lt;script&gt;alert(&quot;a&quot;);&lt;/script&gt;'></a>\n|, render(<<-HAML.unindent)
+      %a{ href: '<script>alert("a");</script>' }
+    HAML
+  end
+
+  specify 'boolean attributes' do
     assert_equal %Q|<span checked='no' disabled></span>\n|, render(<<-HAML.unindent)
       - val = 'no'
       %span{ disabled: true, checked: val, autoplay: false }
     HAML
+  end
+
+  specify 'link_to' do
     assert_equal %Q|<a class="bar" href="#">foo</a>\n|, render(%q|= link_to 'foo', '#', class: 'bar'|)
+  end
+
+  specify 'content_tag' do
     assert_equal <<-HTML.unindent.strip, render(<<-HAML.unindent)
       <div>text
       </div>
@@ -38,6 +49,62 @@ describe Hamlit::RailsTemplate do
       - 3.times do
         = content_tag :div do
           text
+    HAML
+  end
+
+  specify 'find_and_preserve' do
+    assert_equal <<-HTML.unindent, render(<<-'HAML'.unindent)
+      Foo bar
+      &lt;pre&gt;foo bar&lt;/pre&gt;
+      &lt;pre&gt;foo&amp;#x000A;bar&lt;/pre&gt;
+    HTML
+      = find_and_preserve("Foo bar")
+      = find_and_preserve("<pre>foo bar</pre>")
+      = find_and_preserve("<pre>foo\nbar</pre>")
+    HAML
+  end
+
+  specify 'capture_haml' do
+    assert_equal <<-HTML.unindent, render(<<-'HAML'.unindent)
+      <div class='capture'><span>
+      <p>Capture</p>
+      </span>
+      </div>
+    HTML
+      - html = capture_haml do
+        %span
+          %p Capture
+
+      .capture= html
+    HAML
+  end
+
+  specify 'preserve' do
+    assert_equal %q|Foo&amp;#x000A;Bar|, render(<<-'HAML'.unindent)
+      = preserve do
+        Foo
+        Bar
+    HAML
+  end
+
+  specify 'succeed' do
+    assert_equal %Q|<i>succeed</i>&amp;\n|, render(<<-'HAML'.unindent)
+      = succeed '&' do
+        %i succeed
+    HAML
+  end
+
+  specify 'precede' do
+    assert_equal %Q|&amp;<i>precede</i>\n|, render(<<-'HAML'.unindent)
+      = precede '&' do
+        %i precede
+    HAML
+  end
+
+  specify 'surround' do
+    assert_equal %Q|&amp;<i>surround</i>&amp;\n|, render(<<-'HAML'.unindent)
+      = surround '&' do
+        %i surround
     HAML
   end
 end
