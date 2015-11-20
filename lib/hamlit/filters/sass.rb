bf07a5ce589e8b65283628d1cf688edcd8fc8505
Takashi Kokubun <takashikkbn@gmail.com>
2015-11-21 07:31:42 +0900

    Prefer implicit require to support Tilt 1

--- lib/hamlit/filters/sass.rb
+++ lib/hamlit/filters/sass.rb
@@ -2,7 +2,6 @@ module Hamlit
   class Filters
     class Sass < TiltBase
       def compile(node)
-        require 'tilt/sass'
         temple = [:multi]
         temple << [:static, "<style>\n".freeze]
         temple << compile_with_tilt(node, 'sass', indent_width: 2)
--- lib/hamlit/filters/scss.rb
+++ lib/hamlit/filters/scss.rb
@@ -2,7 +2,6 @@ module Hamlit
   class Filters
     class Scss < TiltBase
       def compile(node)
-        require 'tilt/sass'
         temple = [:multi]
         temple << [:static, "<style>\n".freeze]
         temple << compile_with_tilt(node, 'scss', indent_width: 2)
