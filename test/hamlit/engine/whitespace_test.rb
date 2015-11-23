166c738668e16949adae488605c650bb14e5c64f
Takashi Kokubun <takashi-kokubun@cookpad.com>
2015-11-24 01:29:29 +0900

    Split whitespace test

---
+++ test/hamlit/engine/whitespace_test.rb
@@ -0,0 +1,98 @@
+describe Hamlit::Engine do
+  include RenderHelper
+
+  describe 'whitespace removal' do
+    it 'removes outer whitespace by >' do
+      assert_render(<<-HTML.unindent, <<-HAML.unindent)
+        <span>a</span><span>b</span>
+        <span>c</span><span>
+        d
+        </span><span>
+        e
+        </span>
+        <span>f</span>
+      HTML
+        %span> a
+        %span b
+        %span c
+        %span>
+          d
+        %span
+          e
+        %span f
+      HAML
+    end
+
+    it 'removes outer whitespace by > from inside of block' do
+      assert_render(<<-HTML.unindent, <<-HAML.unindent)
+        <span>a</span><span>
+        b
+        </span><span>
+        c
+        </span>
+      HTML
+        %span a
+        - if true
+          %span>
+            b
+        %span
+          c
+      HAML
+    end
+
+    it 'removes whitespaces inside block script' do
+      assert_render(<<-HTML.unindent, <<-HAML.unindent)
+        <span>foofoo2<span>bar</span></span>
+      HTML
+        %span<
+          = 2.times do
+            = 'foo'
+          %span> bar
+      HAML
+    end
+
+    it 'removes whitespace inside script inside silent script' do
+      assert_render(<<-HTML.unindent, <<-HAML.unindent)
+        <div class='bar'>foofoofoo</div>
+      HTML
+        .bar<
+          - 3.times do
+            = 'foo'
+      HAML
+    end
+
+    it 'removes whitespace inside script recursively' do
+      assert_render(<<-HTML.unindent, <<-HAML.unindent)
+        <div class='foo'>bar1bar1bar1bar12</div>
+      HTML
+        .foo<
+          - 1.times do
+            = 2.times do
+              - 2.times do
+                = 1.times do
+                  = 'bar'
+      HAML
+    end
+
+    it 'does not remove whitespace after string interpolation' do
+      assert_render(<<-HTML.unindent, <<-'HAML'.unindent)
+        <div>helloworld</div>
+      HTML
+        %div<
+          #{'hello'}
+          world
+      HAML
+    end
+
+    it 'removes whitespace inside script inside silent script' do
+      assert_render(<<-HTML.unindent, <<-HAML.unindent)
+        <div class='bar'>12</div>
+      HTML
+        .bar<
+          - 1.times do
+            = '1'
+            = '2'
+      HAML
+    end
+  end
+end
