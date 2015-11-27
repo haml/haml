5ddbce1ab06fe05ec26c90b0e7c18aeb038efcaa
Takashi Kokubun <takashi-kokubun@cookpad.com>
2015-11-28 04:50:49 +0900

    Refactor extconf.rb

--- ext/hamlit/extconf.rb
+++ ext/hamlit/extconf.rb
@@ -2,12 +2,12 @@ require 'mkmf'
 
 houdini_dir = File.expand_path('./houdini', __dir__)
 $INCFLAGS << " -I#{houdini_dir}"
-$CFLAGS << ' -Wall -Wextra'
+$CFLAGS   << ' -Wall -Wextra'
 
 $srcs = %w[hamlit.c]
 Dir[File.join(houdini_dir, '*.c')].each do |path|
   src = File.basename(path)
-  FileUtils.ln_s(File.join(houdini_dir, src), src, force: true)
+  FileUtils.ln_s(path, src, force: true)
   $srcs << src
 end
 
