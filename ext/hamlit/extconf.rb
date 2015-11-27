db25217032e9125a29686db0b0bb4a8805dc801e
Takashi Kokubun <takashi-kokubun@cookpad.com>
2015-11-28 01:42:53 +0900

    Compile extension with houdini

--- ext/hamlit/extconf.rb
+++ ext/hamlit/extconf.rb
@@ -1,4 +1,14 @@
 require 'mkmf'
 
+houdini_dir = File.expand_path('./houdini', __dir__)
+$INCFLAGS << " -I#{houdini_dir}"
 $CFLAGS << ' -Wall -Wextra'
+
+$srcs = %w[hamlit.c]
+Dir[File.join(houdini_dir, '*.c')].each do |path|
+  src = File.basename(path)
+  FileUtils.ln_s(File.join(houdini_dir, src), src, force: true)
+  $srcs << src
+end
+
 create_makefile('hamlit/hamlit')
