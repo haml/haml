d381030d9c1eabdf5a8e80b159f8078d368d2cfa
Takashi Kokubun <takashikkbn@gmail.com>
2015-12-12 05:21:59 +0900

    Add a test case for compiling a tag in CLI

---
+++ test/hamlit/cli_test.rb
@@ -0,0 +1,22 @@
+require 'hamlit/cli'
+
+describe Hamlit::CLI do
+  describe '#temple' do
+    def redirect_output
+      out, $stdout = $stdout, StringIO.new
+      yield
+    ensure
+      $stdout = out
+    end
+
+    it 'does not crash when compiling a tag' do
+      redirect_output do
+        Tempfile.create('hamlit') do |f|
+          f.write('%input{ hash }')
+          f.close
+          Hamlit::CLI.new.temple(f.path)
+        end
+      end
+    end
+  end
+end
