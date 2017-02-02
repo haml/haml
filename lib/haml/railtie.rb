0fc0efb743d72527f79580bc49a18d6d93a226d0
Akira Matsuda <ronnie@dio.jp>
2017-02-02 18:39:11 +0900

    Don't load ActionView template handlers before loading action_view

--- lib/haml/railtie.rb
+++ lib/haml/railtie.rb
@@ -10,6 +10,9 @@ if defined?(ActiveSupport)
   ActiveSupport.on_load(:before_initialize) do
     ActiveSupport.on_load(:action_view) do
       require "haml/template"
+
+      require "haml/helpers/safe_erubis_template"
+      Haml::Filters::Erb.template_class = Haml::SafeErubisTemplate
     end
   end
 end
@@ -24,6 +27,3 @@ module Haml
     end
   end
 end
-
-require "haml/helpers/safe_erubis_template"
-Haml::Filters::Erb.template_class = Haml::SafeErubisTemplate
