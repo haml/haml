haml/haml

commit 1a999eae29c9918f037828bfb24f71050b66f55b

Drop legacy optimization code for maintainability
all of those cases can be covered by new Haml::AttributeCompiler!

lib/haml/compiler.rb
@@ -120,42 +120,22 @@ module Haml
         t[:attributes].merge!({"data-trace" => @options.filename.split('/views').last << ":" << @node.line.to_s})
       end
 
-      # Check if we can render the tag directly to text and not process it in the buffer
-      if (object_ref == :nil) && attributes_hashes.empty? && !preserve_script
-        tag_closed = !block_given? && !t[:self_closing] && !parse
+      push_merged_text "<#{t[:name]}", 0
+      push_temple(@attribute_compiler.compile(t[:attributes], object_ref, attributes_hashes))
+      concat_merged_text(
+        if t[:self_closing] && @options.xhtml?
+          " />#{"\n" unless t[:nuke_outer_whitespace]}"
+        else
+          ">#{"\n" unless (t[:self_closing] && @options.html?) ? t[:nuke_outer_whitespace] : (!block_given? || t[:preserve_tag] || t[:nuke_inner_whitespace])}"
+        end)
 
-        open_tag = prerender_tag(t[:name], t[:self_closing], t[:attributes])
-        if tag_closed
-          open_tag << "#{value}</#{t[:name]}>"
-          open_tag << "\n" unless t[:nuke_outer_whitespace]
-        elsif !(parse || t[:nuke_inner_whitespace] ||
-            (t[:self_closing] && t[:nuke_outer_whitespace]))
-          open_tag << "\n"
-        end
-
-        push_merged_text(open_tag,
-          tag_closed || t[:self_closing] || t[:nuke_inner_whitespace] ? 0 : 1)
-
-        @dont_indent_next_line = dont_indent_next_line
-        return if tag_closed
-      else
-        push_merged_text "<#{t[:name]}", 0
-        push_temple(@attribute_compiler.compile(t[:attributes], object_ref, attributes_hashes))
-        concat_merged_text(
-          if t[:self_closing] && @options.xhtml?
-            " />#{"\n" unless t[:nuke_outer_whitespace]}"
-          else
-            ">#{"\n" unless (t[:self_closing] && @options.html?) ? t[:nuke_outer_whitespace] : (!block_given? || t[:preserve_tag] || t[:nuke_inner_whitespace])}"
-          end)
-
-        if value && !parse
-          concat_merged_text("#{value}</#{t[:name]}>#{"\n" unless t[:nuke_outer_whitespace]}")
-        elsif !t[:nuke_inner_whitespace] && !t[:self_closing]
-          @to_merge << [:text, '', 1]
-        end
-
-        @dont_indent_next_line = dont_indent_next_line
+      if value && !parse
+        concat_merged_text("#{value}</#{t[:name]}>#{"\n" unless t[:nuke_outer_whitespace]}")
+      elsif !t[:nuke_inner_whitespace] && !t[:self_closing]
+        @to_merge << [:text, '', 1]
       end
+
+      @dont_indent_next_line = dont_indent_next_line
 
       return if t[:self_closing]
 

commit 914416d3e3e347902d99e1f82f09c7805d990da9

Fix performance regression in 1a999eae29c9918f037828bfb24f71050b66f55b
I've thought `prerender_tag` can be achieved by `Temple::Filters::StaticMerger`.
But StaticMerger requires Temple expression to use [:newline] to merge
:static over newlines.
This commit changes `Haml::Compiler` to generate [:newline].

[Compiled code changes]

* input

```
.foo
.bar
```

* output

before:

```
_hamlout.buffer << ("<div class='foo'></div>\n<div".freeze);;
; _hamlout.buffer << (" class='bar'></div>\n".freeze);
```

after:

```
_hamlout.buffer << ("<div class='foo'></div>\n<div class='bar'></div>\n".freeze);;

```

lib/haml/compiler.rb
@@ -231,8 +231,7 @@ module Haml
 
     def push_temple(temple)
       flush_merged_text
-      newlines = resolve_newlines
-      @temple << [:code, newlines] unless newlines.empty?
+      @temple.concat([[:newline]] * resolve_newlines.count("\n"))
       @temple << temple
       @output_line += TempleLineCounter.count_lines(temple)
     end

lib/haml/generator.rb
@@ -27,6 +27,10 @@ module Haml
       exp
     end
 
+    def on_newline
+      "\n"
+    end
+
     private
 
     def concat(str)