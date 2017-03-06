a113ac2dd62171e065fd1f9c9f364e322bd2d20e
Lukas Oberhuber <lukas.oberhuber@simplybusiness.co.uk>
2017-03-06 11:56:33 +0000

    Do not load modules early when in rails
    Early loading causes a conflict with rspec and view specs.
    
    See issue: https://github.com/haml/haml/issues/883

--- lib/haml/railtie.rb
+++ lib/haml/railtie.rb
@@ -10,13 +10,33 @@ end
 module Haml
   class Railtie < ::Rails::Railtie
     initializer :haml do |app|
-      require "haml/template"
-      if defined?(::Sass::Rails::SassTemplate) && app.config.assets.enabled
-        require "haml/sass_rails_filter"
+      if defined?(ActiveSupport)
+        ActiveSupport.on_load(:before_initialize) do
+          ActiveSupport.on_load(:action_view) do
+            if defined?(::Sass::Rails::SassTemplate) && app.config.assets.enabled
+              require "haml/sass_rails_filter"
+            end
+          end
+        end
+      else
+        require "haml/template"
+        if defined?(::Sass::Rails::SassTemplate) && app.config.assets.enabled
+          require "haml/sass_rails_filter"
+        end
       end
     end
   end
 end
 
-require "haml/helpers/safe_erubis_template"
-Haml::Filters::Erb.template_class = Haml::SafeErubisTemplate
+if defined?(ActiveSupport)
+  require 'haml/template/options'
+  ActiveSupport.on_load(:before_initialize) do
+    ActiveSupport.on_load(:action_view) do
+      require "haml/helpers/safe_erubis_template"
+      Haml::Filters::Erb.template_class = Haml::SafeErubisTemplate
+    end
+  end
+else
+  require "haml/helpers/safe_erubis_template"
+  Haml::Filters::Erb.template_class = Haml::SafeErubisTemplate
+end
