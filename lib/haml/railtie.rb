0821ad5f2aafa4047eae8857ce486641d1ce3f94
Lukas Oberhuber <lukas.oberhuber@simplybusiness.co.uk>
2017-03-06 17:30:03 +0000

    Simplification

--- lib/haml/railtie.rb
+++ lib/haml/railtie.rb
@@ -1,41 +1,19 @@
-if defined?(ActiveSupport)
-  require 'haml/template/options'
-  ActiveSupport.on_load(:before_initialize) do
-    ActiveSupport.on_load(:action_view) do
-      require "haml/template"
-    end
-  end
-end
+require 'haml/template/options'
 
 module Haml
   class Railtie < ::Rails::Railtie
     initializer :haml do |app|
-      if defined?(ActiveSupport)
-        ActiveSupport.on_load(:before_initialize) do
-          ActiveSupport.on_load(:action_view) do
-            if defined?(::Sass::Rails::SassTemplate) && app.config.assets.enabled
-              require "haml/sass_rails_filter"
-            end
-          end
-        end
-      else
+
+      ActiveSupport.on_load(:action_view) do
         require "haml/template"
+
         if defined?(::Sass::Rails::SassTemplate) && app.config.assets.enabled
           require "haml/sass_rails_filter"
         end
+
+        require "haml/helpers/safe_erubis_template"
+        Haml::Filters::Erb.template_class = Haml::SafeErubisTemplate
       end
     end
   end
-end
-
-if defined?(ActiveSupport)
-  ActiveSupport.on_load(:before_initialize) do
-    ActiveSupport.on_load(:action_view) do
-      require "haml/helpers/safe_erubis_template"
-      Haml::Filters::Erb.template_class = Haml::SafeErubisTemplate
-    end
-  end
-else
-  require "haml/helpers/safe_erubis_template"
-  Haml::Filters::Erb.template_class = Haml::SafeErubisTemplate
 end
