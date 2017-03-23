b7de7178d5114877cf536960fcb0fd073dcd14f3
Takashi Kokubun <takashikkbn@gmail.com>
2017-03-23 22:09:44 +0900

    Test options in Rails situation

--- test/options_test.rb
+++ test/options_test.rb
@@ -9,10 +9,11 @@ module Haml
       )
     end
 
-    def test_buffer_defaults_values_are_default_options
+    def test_buffer_defaults_values_are_the_same_as_rails_defaults
+      rails_defaults = Haml::Options.defaults.merge(Haml::Template.options)
       Haml::Options.buffer_option_keys.each do |key|
         assert_equal(
-          Haml::Options.defaults[key],
+          rails_defaults[key],
           Haml::Options.buffer_defaults[key], "key: #{key}"
         )
       end
