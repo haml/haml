5e451f0e29c8511035badc232395cdfaa41d23c8
Takashi Kokubun <takashikkbn@gmail.com>
2017-04-19 02:32:54 +0900

    Fix for the case Rails.application.assets is nil
    cherry-picking @jvenezia's patch in
    https://github.com/haml/haml/pull/868#issuecomment-242689915.

--- lib/haml/sass_rails_filter.rb
+++ lib/haml/sass_rails_filter.rb
@@ -5,8 +5,9 @@ module Haml
     class SassRailsTemplate < ::Sass::Rails::SassTemplate
       if Gem::Version.new(Sprockets::VERSION) >= Gem::Version.new('3.0.0')
         def render(scope=Object.new, locals={}, &block)
-          scope = ::Rails.application.assets.context_class.new(
-            environment: ::Rails.application.assets,
+          environment = ::Sprockets::Railtie.build_environment(Rails.application)
+          scope = environment.context_class.new(
+            environment: environment,
             filename: "/",
             metadata: {}
           )
