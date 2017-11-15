6d783833abb613ff12055cc92b132bded1c5a684
Akira Matsuda <ronnie@dio.jp>
2017-11-16 00:27:11 +0900

    html_escape is not Rails specific

--- test/helper_test.rb
+++ test/helper_test.rb
@@ -338,6 +338,27 @@ MESSAGE
     assert_equal __LINE__ - 6, e.backtrace[0].scan(/:(\d+)/).first.first.to_i
   end
 
+  def test_html_escape
+    assert_equal "&quot;&gt;&lt;&amp;", Haml::Helpers.html_escape('"><&')
+  end
+
+  def test_html_escape_should_work_on_frozen_strings
+    begin
+      assert Haml::Helpers.html_escape('foo'.freeze)
+    rescue => e
+      flunk e.message
+    end
+  end
+
+  def test_html_escape_encoding
+    old_stderr, $stderr = $stderr, StringIO.new
+    string = "\"><&\u00e9" # if you're curious, u00e9 is "LATIN SMALL LETTER E WITH ACUTE"
+    assert_equal "&quot;&gt;&lt;&amp;\u00e9", Haml::Helpers.html_escape(string)
+    assert $stderr.string == "", "html_escape shouldn't generate warnings with UTF-8 strings: #{$stderr.string}"
+  ensure
+    $stderr = old_stderr
+  end
+
   def test_html_escape_non_string
     assert_equal('4.58', Haml::Helpers.html_escape(4.58))
     assert_equal('4.58', Haml::Helpers.html_escape_without_haml_xss(4.58))
