b9e6ea8fc27900177392a5a09d0adb97bf471aac
Takashi Kokubun <takashikkbn@gmail.com>
2018-04-06 10:11:43 +0900

    Drop sample/rails
    because GitHub is so noisy about resolving vulnerable dependencies

--- sample/rails/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  resources :users do
-    collection do
-      get :test
-    end
-  end
-  root to: 'users#index'
-end
--- sample/rails/db/migrate/20151119022746_create_users.rb
+++
@@ -1,8 +0,0 @@
-class CreateUsers < ActiveRecord::Migration
-  def change
-    create_table :users do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
