c7607f45518f1e35de41fbea5ce3428218449948
Takashi Kokubun <takashikkbn@gmail.com>
2021-04-12 21:16:55 -0700

    Force overriding Haml::Plugin
    Close #184

--- lib/hamlit/railtie.rb
+++ lib/hamlit/railtie.rb
@@ -4,6 +4,11 @@ require 'rails'
 module Hamlit
   class Railtie < ::Rails::Railtie
     initializer :hamlit, before: :load_config_initializers do |app|
+      # Load haml/plugin first to override if available
+      begin
+        require 'haml/plugin'
+      rescue LoadError
+      end
       require 'hamlit/rails_template'
     end
   end
