478eaa14af594a4978079bae521b522d47800692
Takashi Kokubun <takashikkbn@gmail.com>
2021-06-06 11:05:15 -0700

    Drop Haml::Buffer dependency from attributes

--- lib/haml/attribute_compiler.rb
+++ lib/haml/attribute_compiler.rb
@@ -51,7 +51,8 @@ module Haml
     # @param dynamic_attributes [Haml::Parser::DynamicAttributes]
     # @return [String] Attributes rendering code
     def compile_runtime_build(attributes, object_ref, dynamic_attributes)
-      "_hamlout.attributes(#{to_literal(attributes)}, #{object_ref}, #{dynamic_attributes.to_literal})"
+      arguments = [@is_html, @attr_wrapper, @escape_attrs, @hyphenate_data_attrs].map(&method(:to_literal)).join(', ')
+      "::Haml::AttributeBuilder.build(#{to_literal(attributes)}, #{object_ref}, #{arguments}, #{dynamic_attributes.to_literal})"
     end
 
     # Build array of grouped values whose sort order may go back and forth, which is also sorted with key name.
@@ -106,7 +107,8 @@ module Haml
       hash_content = values.group_by(&:key).map do |key, values_for_key|
         "#{frozen_string(key)} => #{merged_value(key, values_for_key)}"
       end.join(', ')
-      [:dynamic, "_hamlout.attributes({ #{hash_content} }, nil)"]
+      arguments = [@is_html, @attr_wrapper, @escape_attrs, @hyphenate_data_attrs].map(&method(:to_literal)).join(', ')
+      [:dynamic, "::Haml::AttributeBuilder.build({ #{hash_content} }, nil, #{arguments})"]
     end
 
     # Renders attribute values statically.
