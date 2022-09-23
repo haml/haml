1a1ca00c729fc1e97025397e5a2f0fd57e8e4c4e
Takashi Kokubun <takashikkbn@gmail.com>
2022-09-23 11:05:59 +0900

    Haml doesn't need to override Haml itself

--- lib/haml/railtie.rb
+++ lib/haml/railtie.rb
@@ -4,11 +4,6 @@ require 'rails'
 module Haml
   class Railtie < ::Rails::Railtie
     initializer :haml, before: :load_config_initializers do |app|
-      # Load haml/plugin first to override if available
-      begin
-        require 'haml/plugin'
-      rescue LoadError
-      end
       require 'haml/rails_template'
     end
   end
