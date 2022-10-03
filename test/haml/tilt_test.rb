4067b1a68c13cfd126fc04824e988dadeb572130
Takashi Kokubun <takashikkbn@gmail.com>
2022-10-02 21:44:54 -0700

    Suppress a warning from Tilt
    Haml::TempleEngine: Option :line is invalid

--- test/haml/tilt_test.rb
+++ test/haml/tilt_test.rb
@@ -1,7 +1,16 @@
 describe Tilt::HamlTemplate do
   describe '#render' do
+    def suppress_warning(&block)
+      begin
+        $VERBOSE, verbose = nil, $VERBOSE
+        block.call
+      ensure
+        $VERBOSE = verbose
+      end
+    end
+
     it 'works normally' do
-      template = Tilt::HamlTemplate.new { "%p= name" }
+      template = suppress_warning { Tilt::HamlTemplate.new { "%p= name" } }
       assert_equal "<p>tilt</p>\n", template.render(Object.new, name: 'tilt')
     end
   end
