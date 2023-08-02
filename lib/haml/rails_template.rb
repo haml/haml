3b3c8d209f5a4a849878a920afd031ba2e724706
Corin Langosch <info@corinlangosch.com>
2023-08-02 14:30:19 +0000

    Use the rails template path as filename

--- lib/haml/rails_template.rb
+++ lib/haml/rails_template.rb
@@ -27,6 +27,9 @@ module Haml
       source ||= template.source
       options = RailsTemplate.options
 
+      # Make the filename available in parser etc.
+      options[:filename] = template.identifier
+
       # https://github.com/haml/haml/blob/4.0.7/lib/haml/template/plugin.rb#L19-L20
       # https://github.com/haml/haml/blob/4.0.7/lib/haml/options.rb#L228
       if template.respond_to?(:type) && template.type == 'text/xml'
